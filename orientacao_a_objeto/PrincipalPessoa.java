2881ad17ce900b1606a06d0ee8b9203db5f5b420
Lucas Huzar <142595101+LucasHHuzar@users.noreply.github.com>
2024-04-04 11:15:37 -0300

    Add files via upload

---
+++ orientacao_a_objeto/PrincipalPessoa.java
@@ -0,0 +1,18 @@
+package Aula1Orientacao.Principais;
+
+import Aula1Orientacao.Principais.Encapsulamento.Pessoa;
+
+public class PrincipalPessoa {
+
+    public static void main(String[] args) {
+        
+        //Chamando a classe pessoa para o main
+        Pessoa pessoa = new Pessoa();
+
+        //
+        pessoa.setNome("Lucas");
+        pessoa.setIdade(10);
+
+    }
+    
+}
